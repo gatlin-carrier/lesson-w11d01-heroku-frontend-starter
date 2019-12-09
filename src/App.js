a9ef7b2d5d5a313846fcbf4a44f8b68cf8a809a4
Gatlin Carrier <gatlin.carrier@gmail.com>
2019-12-09 12:13:59 -0500

    added database url

--- src/App.js
+++ src/App.js
@@ -1,35 +1,34 @@
-import React from 'react';
-import './App.css';
-import axios from 'axios';
+import React from "react";
+import "./App.css";
+import axios from "axios";
 // const databaseUrl = process.env.HEROKU_DB_URL || 'http://localhost:3000'
-const databaseUrl = 'https://project3-backend-test.herokuapp.com'
+const databaseUrl = "https://heroku-backend2.herokuapp.com/";
 
 class App extends React.Component {
   state = {
     users: []
-  }
+  };
 
   componentDidMount() {
-    this.getUsers()
+    this.getUsers();
   }
 
   getUsers = () => {
     axios({
       url: `${databaseUrl}/api/users`,
       // url: 'https://project3-backend-test.herokuapp.com/api/users',
-      method: 'get'
-    })
-      .then(users => {
-        console.log(users)
-        this.setState({ users })
-      })
-  }
+      method: "get"
+    }).then(users => {
+      console.log(users);
+      this.setState({ users });
+    });
+  };
 
   render() {
-    console.log(this.state.users)
-    console.log("Rendered")
+    console.log(this.state.users);
+    console.log("Rendered");
     return (
-      <div className="App" >
+      <div className="App">
         <header className="App-header">
           <h1>Project 3 Starter. Let's git it!</h1>
         </header>
